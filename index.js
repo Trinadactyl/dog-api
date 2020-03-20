f1780dd51ebc8271fd48114748503bac0b1a24ca
Trina <trinamceary@gmail.com>
2020-03-20 12:26:13 -0700

    complete main functions, basic styling

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 'use strict'
 
 function getDogImage(){
-
+  fetch('https://dog.ceo/api/breeds/image/random')
+  .then(response => response.json())
+  .then(responseJson => 
+    displayResults(responseJson))
+  .catch(error => alert('Oops. Something went wong')
+  );
 }
 
-function displayResults(){
-
+function displayResults(responseJson){
+  console.log(responseJson);
+  $('.results-img').replaceWith(
+    `<img src=${responseJson.message} class="results-img">`
+  )
+  $('.results').removeClass('hidden');
 }
 
 function watchForm(){
-  
+  $('form').submit(event => {
+    event.preventDefault();
+    getDogImage();
+  });
 }
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
